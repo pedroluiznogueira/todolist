edf3e34e9f595e135bd05b710c0656db0dbc8313
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-07 01:00:55 -0300

    Estruturando componentes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefaComponent } from './components/tarefa/tarefa.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefaComponent,
+    ListaComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
